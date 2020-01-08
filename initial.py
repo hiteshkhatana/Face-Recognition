23735ca408085cb24baa2a96dce5f74fbb888c46
hiteshkhatana <47058253+hiteshkhatana@users.noreply.github.com>
2020-01-08 17:31:59 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -15,22 +15,21 @@ check = True
 while check == True:
 	c ,frame = video.read()
 
-	gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
-
-	faces = face_cascade.detectMultiScale(gray , scaleFactor = 1.05 , minNeighbors = 5)
+	faces = face_cascade.detectMultiScale(frame , scaleFactor = 1.05 , minNeighbors = 5)
 
 	for x,y,w,h in faces:
-		cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),3)
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 		font = cv2.FONT_HERSHEY_PLAIN
-		cv2.putText(gray, "Press C to capture",(x,y-20),font,1,(255,255,255) )
+		cv2.putText(frame, "Press C to capture",(x,y-20),font,1,(255,255,255) )
+		cropped = frame[y:y+h , x:x+w]
 
 	key = cv2.waitKey(1)
 	if key == ord('c'):
 		check = False
 		video.release()
 		cv2.destroyAllWindows()
-		ans = Identify(frame)
-	cv2.imshow('capturing',gray )
+		ans = Identify(cropped)
+	cv2.imshow('capturing',frame )
 
 			
 
